40560b084e8e3b62473742d4d1d3d1a62505befb
Ranadeep Biswas <ranadip.bswas@gmail.com>
2016-12-16 00:15:59 +0530

    fixes

--- runall.sh
+++ runall.sh
@@ -2,7 +2,7 @@
 
 n=$1
 
-for s in b w d r c; do
+for s in b w r d c; do
 	./gen_cnf $n > board_$n.cnf
 	command time -f "elapsed time %e" ./test $s board_$n.cnf board_$n.sol
 	./verify board_$n.cnf board_$n.sol
